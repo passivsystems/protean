c8fff5ce2c35dfc9236ce27912533a13c56c81d9
Ross McDonald <Ross.McDonald@passivsystems.com>
2014-04-25 11:51:06 +0100

    refactor docs functions

--- src/protean/transformations/docs.clj
+++ src/protean/transformations/docs.clj
@@ -41,38 +41,27 @@
   (l/id="project-version") (l/content (get-version))
   (l/id="projects-list") (l/content (build-projects-tr payload)))
 
-(defn build-headers-list [payload]
-  (map #(li (str (key %) ": " (val %))) payload))
-
-(defn build-req-params-list [payload]
-  (map #(li (str (key %) " = " (val %))) payload))
-
-(defn build-form-keys-list [payload]
-  (map #(li (str (key %) ": " (val %))) (:form-keys payload)))
-
-(defn build-json-keys-list [payload]
-  (map #(li (str (key %) ": " (val %))) (:body-keys payload)))
-
-(defn build-project-td [payload]
+(defn project-td
+  [{:keys [method doc headers req-params body-keys form-keys uri] :as payload}]
   (vec [(td
          (ul-unstyled
-          (vec [(li (upper-case (name (:method payload))))
+          (vec [(li (upper-case (name method)))
                 (li
                   (l/node :span
                           :attrs {:class "glyphicon glyphicon-info-sign"
-                                  :title (or (:doc payload) "")}))])))
+                                  :title (or doc "")}))])))
         (l/node :td :attrs {:width "500px"} :content
           (ul-unstyled
-              (vec [(li (:uri payload))
+              (vec [(li uri)
                     (l/node :div
                       :attrs {:class "panel panel-info"}
                       :content (li (small (txc/curly-> payload))))])))
-        (td (ul-unstyled (build-headers-list (:headers payload))))
-        (td (ul-unstyled (build-req-params-list (:req-params payload))))
-        (td (ul-unstyled (build-form-keys-list payload)))
-        (td (ul-unstyled (build-json-keys-list payload)))]))
+        (td (ul-unstyled (for [[k v] headers] (li (str k ":" v)))))
+        (td (ul-unstyled (for [[k v] req-params] (li (str k "=" v)))))
+        (td (ul-unstyled (for [[k v] form-keys] (li (str k "=" v)))))
+        (td (ul-unstyled (for [[k v] body-keys] (li (str k ":" v)))))]))
 
-(defn build-project-tr [payload] (map #(tr (build-project-td %)) payload))
+(defn build-project-tr [payload] (map #(tr (project-td %)) payload))
 
 (l/defdocument project-template (file "public/html/project.html")
   [id payload]
