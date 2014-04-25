5b93f4d9e986d8f39d9ae824307841ac72b77624
Ross McDonald <Ross.McDonald@passivsystems.com>
2014-04-25 15:14:56 +0100

    refactor element building

--- src/protean/transformations/docs.clj
+++ src/protean/transformations/docs.clj
@@ -18,54 +18,57 @@
 ;; Helper functions and data
 ;; =============================================================================
 
-(defmacro get-version []
-  (System/getProperty "protean.version"))
+(defmacro get-version [] (System/getProperty "protean.version"))
 
+(defonce glyph-info "glyphicon glyphicon-info-sign")
+(defonce pnl-info "panel panel-info")
 (defn a [h c] (l/node :a :attrs {:href h} :content c))
 (defn ul-unstyled [d] (l/node :ul :attrs {:class "list-unstyled"} :content d))
 (defn li [d] (l/node :li :content d))
 (defn tr [d] (l/node :tr :content d))
 (defn td [d] (l/node :td :content d))
+(defn div ([] (l/node :div)) ([d] (l/node :div :content d)))
+(defn span ([] (l/node :span)) ([d] (l/node :span :content d)))
 (defn small [d] (l/node :small :content d))
 (defn <- [d] (l/content d))
+(defn clazz [classes payload] (assoc-in payload [:attrs :class] classes))
+(defn title [title payload] (assoc-in payload [:attrs :title] title))
+(defn width [width payload] (assoc-in payload [:attrs :width] width))
 
 
 ;; =============================================================================
 ;; Transformation functions
 ;; =============================================================================
 
-(defn build-projects-tr [payload]
+(defn projects-tr [payload]
   (map #(li (a (str "/documentation/projects/" (name %)) (name %))) payload))
 
 (l/defdocument projects-template (file "public/html/projects.html")
   [payload]
   (l/id="project-version") (<- (get-version))
-  (l/id="projects-list") (<- (build-projects-tr payload)))
+  (l/id="projects-list") (<- (projects-tr payload)))
 
 (defn project-td
   [{:keys [method doc headers req-params body-keys form-keys uri] :as payload}]
   (vec [(td
-         (ul-unstyled
-          (vec [(li (upper-case (name method)))
-                (li
-                  (l/node :span
-                          :attrs {:class "glyphicon glyphicon-info-sign"
-                                  :title (or doc "")}))])))
-        (l/node :td :attrs {:width "500px"} :content
           (ul-unstyled
-              (vec [(li uri)
-                    (l/node :div
-                      :attrs {:class "panel panel-info"}
-                      :content (li (small (txc/curly-> payload))))])))
+            (vec [(li (upper-case (name method)))
+                  (li (->> (span) (clazz glyph-info) (title (or doc ""))))])))
+        (->> (td
+               (ul-unstyled
+                (vec [(li uri)
+                      (li (->> (div (small (txc/curly-> payload)))
+                               (clazz pnl-info)))])))
+             (width "500px"))
         (td (ul-unstyled (for [[k v] headers] (li (str k ":" v)))))
         (td (ul-unstyled (for [[k v] req-params] (li (str k "=" v)))))
         (td (ul-unstyled (for [[k v] form-keys] (li (str k "=" v)))))
         (td (ul-unstyled (for [[k v] body-keys] (li (str k ":" v)))))]))
 
-(defn build-project-tr [payload] (map #(tr (project-td %)) payload))
+(defn project-tr [payload] (map #(tr (project-td %)) payload))
 
 (l/defdocument project-template (file "public/html/project.html")
   [id payload]
   (l/id="project-version") (<- (get-version))
   (l/id="project-name") (<- (name id))
-  (l/element= :tbody) (<- (build-project-tr payload)))
+  (l/element= :tbody) (<- (project-tr payload)))
