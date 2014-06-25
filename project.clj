4ed7e9c3309106999aed93e00f84cc7d67fef0b2
rossputin <rossajmcd@gmail.com>
2014-06-25 14:46:50 +0100

    bump version to 0.8.0-pre.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protean "0.9.0-SNAPSHOT"
+(defproject protean "0.8.0-pre.3"
   :description "Simulate RESTful API's, easily"
   :url "http://github.com/passivsystems/protean"
   :license {:name "Apache License v2.0"
