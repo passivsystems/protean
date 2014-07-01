cae72b95bc8e1ea9fad227ec0c7590ced902fd9c
Ross McDonald <Ross.McDonald@passivsystems.com>
2014-07-01 14:24:49 +0100

    bump version to 0.8.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protean "0.8.0"
+(defproject protean "0.8.1"
   :description "Take control of your RESTful API's, simulate, doc, test easily."
   :url "http://github.com/passivsystems/protean"
   :license {:name "Apache License v2.0"
