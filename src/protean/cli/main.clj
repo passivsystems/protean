e104355cd34e785ef3735fd74f35bb11c2abe8bb
rossputin <rossajmcd@gmail.com>
2014-08-18 16:55:10 +0100

    Revert a546f94..c241797
    This rolls back to commit a546f94cb525ea958fefb45d223665c861571a8f.

--- src/protean/cli/main.clj
+++ src/protean/cli/main.clj
@@ -36,6 +36,7 @@
     "N/A"))
 
 (defn- codices->silk [f n d]
+  (println (aa/bold-green "Generating documentation..."))
   (let [codices (edn/read-string (slurp f))
         locs {"locs" (if n (vector n) n)}
         an (pta/analysis-> "host" 1234 codices locs)]
@@ -49,8 +50,6 @@
 
 (defn- test-sim [h p f b]
   (println (aa/bold-green "Testing simulation..."))
-  (println "b : " b)
-  (println "locs : " (:locs b))
   (let [codices (edn/read-string (slurp f))
         br (b/visit b codices)
         tests (tc/clj-httpify h p codices b)
