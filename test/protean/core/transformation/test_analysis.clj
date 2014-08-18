a546f94cb525ea958fefb45d223665c861571a8f
rossputin <rossajmcd@gmail.com>
2014-08-18 11:07:13 +0100

    add tests and test data from core project

---
+++ test/protean/core/transformation/test_analysis.clj
@@ -0,0 +1,25 @@
+(ns protean.core.transformation.test-analysis
+  (:require [protean.core.io.data :as d])
+  (:use [clojure.test]
+        [protean.core.transformation.analysis :only (analysis->)]))
+
+(deftest simple-analysis
+  (let [corpus {"locs" ["sample simple"]}
+        aSis (analysis-> "localhost" 8080 (d/read-edn "get-codex.edn") corpus)
+        an (first aSis)]
+    (is (= :get (:method an)))
+    (is (= "http://localhost:8080/sample/simple" (:uri an)))
+    (is (= nil (get-in an [:codex :body-res])))
+    (is (= nil (get-in an [:codex :success-code])))
+    (is (= nil (get-in an [:codex :content-type])))))
+
+(deftest simulation-analysis
+  (let [corpus {"locs" ["sim path1"]}
+        aSis (analysis-> "localhost" 8080 (d/read-edn "sim.edn") corpus)
+        an (first aSis)]
+    (is (= :get (:method an)))
+    (is (= "http://localhost:8080/sim/path1" (:uri an)))
+    (is (= (count (get-in an [:codex :body])) 2))
+    (is (= nil (get-in an [:codex :body-res])))
+    (is (= nil (get-in an [:codex :success-code])))
+    (is (= nil (get-in an [:codex :content-type])))))
