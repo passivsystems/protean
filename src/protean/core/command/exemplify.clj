3fffd5b7fabe69181e333fda131404aaec90b8ed
rossputin <rossajmcd@gmail.com>
2014-08-21 14:29:58 +0100

    improve ns comment

--- src/protean/core/command/exemplify.clj
+++ src/protean/core/command/exemplify.clj
@@ -1,6 +1,9 @@
 (ns protean.core.command.exemplify
   "Replace plapceholder values with codex provided examples
-   for given types.
+   for some more complex types.
+
+   N.B. we are more likely to use seed or generated examples
+   for simple (language provided) types.
 
    This translation task occurs after seeding has completed
    (probably while some nodes are not visitable)."
