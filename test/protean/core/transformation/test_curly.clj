fd9f747182bcfb7523ba9bf2363222ad686b9ee1
rossputin <rossajmcd@gmail.com>
2014-08-26 15:56:19 +0100

    improve curl analysis transformation test

--- test/protean/core/transformation/test_curly.clj
+++ test/protean/core/transformation/test_curly.clj
@@ -1,10 +1,14 @@
 (ns protean.core.transformation.test-curly
-  (:require [protean.core.io.data :as d]
+  (:require [clojure.string :refer [split]]
+            [protean.core.io.data :as d]
             [protean.core.transformation.curly :refer [curly-analysis->]]
             [clojure.test :refer :all]))
 
 (deftest simple-curly
   (let [codices (d/read-edn "get-codex.edn")
         curly (curly-analysis-> "localhost" 8080 codices "sample")
-        c (first curly)]
-    (is (= true (.contains c "curl")))))
+        c (first curly)
+        s (split c #" ")]
+    (is (= (first s) "curl"))
+    (is (= (second s) "-v"))
+    (is (= (last s) "'http://localhost:8080/sample/simple'"))))
