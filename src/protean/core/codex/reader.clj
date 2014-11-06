e1e38e81125e9c512460f57bb90830f1e12342ec
colin-passiv <colin-passiv@users.noreply.github.com>
2014-11-06 16:34:07 +0000

    Reader supports :includes at any level

--- src/protean/core/codex/reader.clj
+++ src/protean/core/codex/reader.clj
@@ -4,6 +4,12 @@
 (defn read-codex
   "will read the codex eden file, merging with any referenced files"
   [file]
-  (let [read (edn/read-string (slurp file))
-        include-edn (map read-codex (:includes read))]
-    (merge (reduce merge include-edn) read)))
+  (defn- merge-includes [[k v]]
+    (cond
+      (= :includes k) (reduce merge (map read-codex v))
+      (map? v) {k (apply merge (map merge-includes v))}
+      :else {k v}))
+  (let [read (edn/read-string (slurp file))]
+    (apply merge (map merge-includes read))))
+
+
