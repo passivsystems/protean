c14999928317a27917fca2caa649490bcd31c3c3
rossputin <rossajmcd@gmail.com>
2014-11-06 21:47:33 +0000

    fix key processing bug introduced by codex re-structuring

--- src/protean/core/transformation/sim.clj
+++ src/protean/core/transformation/sim.clj
@@ -39,7 +39,7 @@
 (defn- service-path? [codices srv k]
   (or (get-in codices [srv k])
       (get-in codices [srv (wild-path? k
-    (filter #(.contains % "*") (keys (get-in codices [srv]))))])))
+    (filter #(.contains % "*") (d/custom-keys (get-in codices [srv]))))])))
 
 (defn req-> [{:keys [request-method headers query-params form-params body]}]
   {:method request-method :hdrs headers :q-params query-params
