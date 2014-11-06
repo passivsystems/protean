36dafbaab43e642d35a5f7df585e529e55b31018
colin-passiv <colin-passiv@users.noreply.github.com>
2014-11-06 15:36:28 +0000

    Supporting :includes at top level in edn file

--- src/protean/core/codex/reader.clj
+++ src/protean/core/codex/reader.clj
@@ -1,8 +1,9 @@
 (ns protean.core.codex.reader
   (:require [clojure.edn :as edn]))
 
-;      (reset! pipe/state (merge @pipe/state (r/read f)))))
 (defn read-codex
   "will read the codex eden file, merging with any referenced files"
   [file]
-  (edn/read-string (slurp file)))
+  (let [read (edn/read-string (slurp file))
+        include-edn (map read-codex (:includes read))]
+    (merge (reduce merge include-edn) read)))
