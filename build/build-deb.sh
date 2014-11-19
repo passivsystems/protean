dae4d09d268081ec866fe2efd58fdc169f58136d
rossputin <rossajmcd@gmail.com>
2014-11-19 15:50:11 +0000

    add codex data to DEB build

--- build/build-deb.sh
+++ build/build-deb.sh
@@ -20,6 +20,7 @@ then
     cp ../sample-petstore.sim.edn ../target/deb/usr/lib/protean/sample-petstore.sim.edn
     cp ../protean-utils.cod.edn ../target/deb/usr/lib/protean/protean-utils.cod.edn
     cp ../protean-utils.sim.edn ../target/deb/usr/lib/protean/protean-utils.sim.edn
+    cp -r ../test-data ../target/deb/usr/lib/protean/test-data
     cp -r ../public ../target/deb/usr/lib/protean
     cp -r etc/protean ../target/deb/usr/bin
     cp -r etc/protean-server ../target/deb/usr/bin
