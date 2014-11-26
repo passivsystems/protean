cb1700e0df66b7a8cae5b77769c7ae7c6866f697
rossputin <rossajmcd@gmail.com>
2014-11-26 11:02:32 +0000

    fix formatting

--- test/protean/core/transformation/test_curly.clj
+++ test/protean/core/transformation/test_curly.clj
@@ -3,11 +3,18 @@
             [protean.core.transformation.curly :refer [curly-analysis->]]
             [expectations :refer :all]))
 
+;; =============================================================================
+;; Testing transformation from analysis structure to curl command
+;; =============================================================================
+
 (let [analysed '(
   {
    :method :get
    :uri "http://laton.lan:3000/sample/simple"
-   :tree [nil {:get nil} {"simple" {:get nil}} {:rsp {:200 {:doc "OK"}}} {"sample" {"simple" {:get nil}} :get {:rsp {:200 {:doc "OK"}}}}]
+   :tree [nil
+          {:get nil}
+          {"simple" {:get nil}} {:rsp {:200 {:doc "OK"}}}
+          {"sample" {"simple" {:get nil}} :get {:rsp {:200 {:doc "OK"}}}}]
   }
   )
       [cmd verbosity uri] (split (first (curly-analysis-> analysed)) #" ")]
