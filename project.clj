c04bc7933c90559ff28df96da2fedf3ee99e4705
rossputin <rossajmcd@gmail.com>
2014-12-06 19:37:02 +0000

    upgrade deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protean "0.9.0-pre.7"
+(defproject protean "0.9.0-alpha.1"
   :description "Take control of your RESTful API's, simulate, doc, test easily."
   :url "http://github.com/passivsystems/protean"
   :license {:name "Apache License v2.0"
@@ -9,17 +9,17 @@
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.namespace "0.2.7"]
-                 [ring/ring-jetty-adapter "1.3.1"]
-                 [ring/ring-core "1.3.1"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [ring/ring-core "1.3.2"]
                  [ring/ring-codec "1.0.0"]
-                 [compojure "1.2.1"]
+                 [compojure "1.3.1"]
                  [cheshire "5.3.1"]
                  [environ "1.0.0"]
                  [com.taoensso/timbre "3.3.1"]
                  [clj-http "1.0.1"]
                  [io.aviso/pretty "0.1.13"]
                  [com.novemberain/pantomime "2.3.0"]
-                 [expectations "2.0.9"]
+                 [expectations "2.0.13"]
                  [me.raynes/laser "1.1.1"]
                  [me.raynes/hickory "0.4.2"]
                  [me.rossputin/diskops "0.2.1"]
