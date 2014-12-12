868c6491b371e97de6626cb59eae85e6375e62e1
rossputin <rossajmcd@gmail.com>
2014-12-12 15:51:41 +0000

    release 0.9.0-alpha.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protean "0.9.0-alpha.3"
+(defproject protean "0.9.0-alpha.4"
   :description "Take control of your RESTful API's, simulate, doc, test easily."
   :url "http://github.com/passivsystems/protean"
   :license {:name "Apache License v2.0"
