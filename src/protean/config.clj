53932cf7b45872fb9860ae37ec453163096340ae
rossputin <rossajmcd@gmail.com>
2014-12-17 10:22:04 +0000

    #84 namespace protect env vars

--- src/protean/config.clj
+++ src/protean/config.clj
@@ -10,13 +10,13 @@
 
 (defonce host (or (env :hostname) (.getCanonicalHostName (java.net.InetAddress/getLocalHost))))
 
-(defn sim-port [] (or (env :sim-port) "3000"))
+(defn sim-port [] (or (env :protean-sim-port) "3000"))
 
-(defn admin-port [] (or (env :admin-port) "3001"))
+(defn admin-port [] (or (env :protean-admin-port) "3001"))
 
-(defn codex-dir [] (or (env :codex-dir) (d/pwd)))
+(defn codex-dir [] (or (env :protean-codex-dir) (d/pwd)))
 
-(defn asset-dir [] (or (env :asset-dir) "public"))
+(defn asset-dir [] (or (env :protean-asset-dir) "public"))
 
 (defn log-level [] (keyword (or (env :protean-log-level) "info")))
 
