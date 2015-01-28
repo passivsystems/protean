2ce6fb0fb2ec7a1c1c50eceef6137179f4a7f90a
colin-passiv <colin-passiv@users.noreply.github.com>
2015-01-28 16:57:02 +0000

    Added clj-time

--- project.clj
+++ project.clj
@@ -30,7 +30,8 @@
                  [org.slf4j/slf4j-simple "1.7.7"]
                  [overtone/at-at "1.2.0"]
                  [com.github.fge/json-schema-validator "2.1.7"]
-                 [aysylu/loom "0.5.0"]]
+                 [aysylu/loom "0.5.0"]
+                 [clj-time "0.9.0"]]
   :plugins [[lein-ring "0.8.10"]
             [lein-expectations "0.0.7"]]
   :aot :all
