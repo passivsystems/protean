6a37c625bd25e549483cb635cbe6841091e97978
colin-passiv <colin-passiv@users.noreply.github.com>
2015-02-27 11:39:22 +0000

    fixed reader resolution of referenced files

--- src/protean/core/codex/reader.clj
+++ src/protean/core/codex/reader.clj
@@ -4,18 +4,17 @@
             [protean.core.codex.document :as d])
   (:import java.io.File))
 
-(defn- merge-includes [codex-dir [k v]]
-  (cond
-    (= :includes k) (reduce merge-with merge (map (partial read-codex-part codex-dir) v))
-    (map? v) {k (apply merge-with merge (map (partial merge-includes codex-dir) v))}
-    :else {k v}))
-
 (defn- read-codex-part
   "will read the codex eden file, merging with any referenced files"
   [codex-dir file]
+  (defn- merge-includes [[k v]]
+    (cond
+      (= :includes k) (reduce merge-with merge (map (partial read-codex-part codex-dir) v))
+      (map? v) {k (apply merge-with merge (map merge-includes v))}
+      :else {k v}))
   (let [afile (if (string? file) (d/to-path-dir file codex-dir) file)
         read (edn/read-string (slurp afile))]
-    (apply merge-with merge (map (partial merge-includes codex-dir) read))))
+    (apply merge-with merge (map merge-includes read))))
 
 (defn read-codex
   "will read the codex eden file, merging with any referenced files"
