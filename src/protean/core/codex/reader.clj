cd790eeb42242f677b9d33df15dc698a84c5fa23
colin-passiv <colin-passiv@users.noreply.github.com>
2015-02-27 11:17:54 +0000

    fixed reader resolution of referenced files

--- src/protean/core/codex/reader.clj
+++ src/protean/core/codex/reader.clj
@@ -1,20 +1,21 @@
 (ns protean.core.codex.reader
   (:require [clojure.edn :as edn]
-            [clojure.java.io :as io]
-            [protean.config :as conf])
+            [protean.config :as conf]
+            [protean.core.codex.document :as d])
   (:import java.io.File))
+
+(defn- merge-includes [codex-dir [k v]]
+  (cond
+    (= :includes k) (reduce merge-with merge (map (partial read-codex-part codex-dir) v))
+    (map? v) {k (apply merge-with merge (map (partial merge-includes codex-dir) v))}
+    :else {k v}))
 
 (defn- read-codex-part
   "will read the codex eden file, merging with any referenced files"
-  [file]
-  (defn- merge-includes [[k v]]
-    (cond
-      (= :includes k) (reduce merge-with merge (map read-codex-part v))
-      (map? v) {k (apply merge-with merge (map merge-includes v))}
-      :else {k v}))
-  (let [afile (if (string? file) (io/file (conf/codex-dir) file) file)
+  [codex-dir file]
+  (let [afile (if (string? file) (d/to-path-dir file codex-dir) file)
         read (edn/read-string (slurp afile))]
-    (apply merge-with merge (map merge-includes read))))
+    (apply merge-with merge (map (partial merge-includes codex-dir) read))))
 
 (defn read-codex
   "will read the codex eden file, merging with any referenced files"
@@ -22,4 +23,4 @@
   (let [codex-dir (.getParent (.getAbsoluteFile file))]
     ;; TODO review this - alternatives are setting a binding, updating system
     ;; property (env caches values at startup)?
-    (merge {:codex-dir codex-dir} (read-codex-part file))))
+    (merge {:codex-dir codex-dir} (read-codex-part file codex-dir))))
