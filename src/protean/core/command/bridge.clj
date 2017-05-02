89eda4c6403b55d0e1cbe32e560b5279d8cac06a
Nick Featch <Nick.Featch@passivsystems.com>
2017-05-02 11:26:12 +0100

    revert sorting change only from previous commit

--- src/protean/core/command/bridge.clj
+++ src/protean/core/command/bridge.clj
@@ -59,7 +59,7 @@
   [{:keys [host port locs commands seed] :as corpus} codices]
   (doseq [cmd commands]
     (pb/config cmd corpus)
-    (let [paths (p/paths codices locs)
+    (let [paths (sort-by (juxt :path :method) (p/paths codices locs)) ;; otherwise unsorted EDN map
           probes (filter some? (doall (map-indexed (fn [idx itm] (build cmd corpus idx itm)) paths)))
           results (pb/dispatch cmd corpus probes)]
       (pb/analyse cmd corpus results))))
