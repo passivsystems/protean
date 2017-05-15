6e91881a3c7aa6ac9256e51a544b43ae9498ae2a
rossputin <rossajmcd@gmail.com>
2017-05-16 00:30:07 +0100

    add tests to test-reader

--- test/protean/core/codex/test_reader.clj
+++ test/protean/core/codex/test_reader.clj
@@ -2,4 +2,8 @@
   (:require [protean.core.io.data :as d]
             [expectations :refer :all]))
 
-(let [cdx (d/read-edn "sim.edn")] (expect 8 (count (:ordered-resources cdx))))
+(let [cdx (d/read-edn "reader.edn")
+      odr (:ordered-resources cdx)]
+  (expect 12 (count odr))
+  (expect "cpath-1" (first odr))
+  (expect "bpath_2" (second odr)))
