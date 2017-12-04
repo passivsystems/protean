be52c9d5dea4b48070d5193f9666512e6df57c78
colin-passiv <colin-passiv@users.noreply.github.com>
2017-12-04 12:38:25 +0000

    download existing tarball for nix installation

--- build/build-nix.sh
+++ build/build-nix.sh
@@ -6,33 +6,9 @@
 # Ross McDonald <ross@bheap.co.uk>
 #
 
-# Assumes that lein uberjar has been run and uberjar exists in target directory
-ROOT_DIR=$(dirname $0)/..
-TARGET_DIR=$ROOT_DIR/target/nix/protean-0.12.0-pre.1
-
-
-
-# Assumes that lein uberjar has been run and uberjar exists in target directory
-rm -rf $ROOT_DIR/target/nix
-mkdir -p $TARGET_DIR/lib
-
-cp $ROOT_DIR/target/*standalone* $TARGET_DIR/lib/protean.jar
-cp $ROOT_DIR/defaults.edn TARGET_DIR/lib/defaults.edn
-cp $ROOT_DIR/sample-petstore.cod.edn $TARGET_DIR/lib/sample-petstore.cod.edn
-cp $ROOT_DIR/protean-utils.cod.edn $TARGET_DIR/lib/protean-utils.cod.edn
-cp $ROOT_DIR/protean-utils.sim.edn $TARGET_DIR/lib/protean-utils.sim.edn
-rm -rf $ROOT_DIR/silk_templates/data/protean-api/*
-rm -rf $ROOT_DIR/silk_templates/site/*
-cp -r $ROOT_DIR/silk_templates $TARGET_DIR/lib/silk_templates
-cp -r $ROOT_DIR/test-data $TARGET_DIR/lib/test-data
-cp -r $ROOT_DIR/public $TARGET_DIR/lib
-
-cd $ROOT_DIR/target/nix
-tar cvzf protean-0.12.0-pre.1.tgz *
-cd -
 
 nix-build --keep-failed --expr 'with import <nixpkgs> {}; callPackage ./nix/default.nix {}'
 
 # how to delete build after successful build?
 # currently: rm result; nix-store --gc
-# answer: nix-store --delete /nix/store/path
+# answer: nix-store --delete `readlink result` --ignore-liveness
--- build/nix/default.nix
+++ build/nix/default.nix
@@ -1,24 +1,30 @@
-{ stdenv, ant }:
+{ stdenv, fetchurl, ant }:
 
 let
-  version = "0.12.0-pre.1";
+  version = "0.11.0";
 in
 stdenv.mkDerivation rec {
   name = "protean-${version}";
-#  src = fetchurl {
-#      url = "https://github.com/passivsystems/protean/releases/download/0.11.0/protean-nix-${version}.tgz";
-#      sha256 = "1fr3sw06qncb6yygcf2lbnkxma4v1dbigpf39ajrm0isxbpyv944"; # TODO set value, once zip defined
-#  };
+  src = fetchurl {
+    url = "https://github.com/passivsystems/protean/releases/download/${version}/protean.tgz";
+    sha256 = "ac7d6bc830515e5a959f66535a5dd82a4d9391ba0cc7643bb17499b59b2cadc7"; # https://md5file.com/calculator
+  };
 
-  src = "/home/colin/Workspaces/clojure/protean/target/nix/protean-${version}.tgz";
+  # required since tarball has multiple root directories, and cannot be assigned automatically
+  # (even though we're not using it)
+  sourceRoot = ".";
 
+  # Phases: https://nixos.wiki/wiki/Create_and_debug_nix_packages#Using_nix-shell_for_package_development
   installPhase = ''
-    mkdir $out
-    cp ./* $out/ -R
+    echo "installPhase"
+    mkdir -p $out/lib $out/bin
+    cp ./* $out/lib -R
+
+    # remove invalid executables
+    rm $out/lib/protean
+    rm $out/lib/protean-server
 
     # create executables
-    mkdir -p $out/bin
-
     echo "#!/bin/bash
     export PROTEAN_HOME=$out/lib/protean
     export PROTEAN_CODEX_DIR=$out/lib/protean
