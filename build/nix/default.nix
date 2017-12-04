c79f2fbeeae1ca861f70b8a14598975fd4238aca
colin-passiv <colin-passiv@users.noreply.github.com>
2017-12-04 13:01:06 +0000

    create unpack root directory

--- build/nix/default.nix
+++ build/nix/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, ant }:
+{ stdenv, fetchurl, jre }:
 
 let
   version = "0.11.0";
@@ -10,14 +10,21 @@ stdenv.mkDerivation rec {
     sha256 = "ac7d6bc830515e5a959f66535a5dd82a4d9391ba0cc7643bb17499b59b2cadc7"; # https://md5file.com/calculator
   };
 
-  # required since tarball has multiple root directories, and cannot be assigned automatically
-  # (even though we're not using it)
-  sourceRoot = ".";
+  # downloaded tarball doesn't have a root directory - move into one while unpacking.
+  # we could use sourceRoot=".", but copying in installPhase will also copy nix files (e.g. env-var)
+  unpackPhase = ''
+    echo "unpackPhase"
+    runHook preUnpack
+    mkdir protean-${version}
+    tar -C protean-${version} -xvzf $src
+    export sourceRoot="protean-${version}"
+    runHook postUnpack
+  '';
 
   # Phases: https://nixos.wiki/wiki/Create_and_debug_nix_packages#Using_nix-shell_for_package_development
   installPhase = ''
     echo "installPhase"
-    mkdir -p $out/lib $out/bin
+    mkdir -p $out/{lib,bin}
     cp ./* $out/lib -R
 
     # remove invalid executables
@@ -28,12 +35,12 @@ stdenv.mkDerivation rec {
     echo "#!/bin/bash
     export PROTEAN_HOME=$out/lib/protean
     export PROTEAN_CODEX_DIR=$out/lib/protean
-    java -Xmx64m -jar $out/lib/protean.jar \"\$@\"
+    ${jre}/bin/java -Xmx64m -jar $out/lib/protean.jar \"\$@\"
     " > $out/bin/protean
     chmod +x $out/bin/protean
 
     echo "#!/bin/bash
-    java -cp $out/lib/protean.jar -Xmx32m protean.server.main
+    ${jre}/bin/java -cp $out/lib/protean.jar -Xmx32m protean.server.main
     " > $out/bin/protean-server
     chmod +x $out/bin/protean-server
   '';
@@ -41,9 +48,7 @@ stdenv.mkDerivation rec {
   meta = {
     description = "Evolve your RESTful API's and Web Services.";
     homepage = "https://github.com/passivsystems/protean";
-    maintainers = [ stdenv.lib.maintainers.cmcdragonkai ];
     license = stdenv.lib.licenses.asl20;
-    priority = 1;
-    platforms = stdenv.lib.platforms.linux;
+    platforms = stdenv.lib.platforms.all;
   };
 }
